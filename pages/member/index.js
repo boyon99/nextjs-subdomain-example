56bdb33942e0a34c03f0436e86ec93f781e5a5ec
boyon99 <bo_yon_99@naver.com>
2023-06-14 15:05:08 +0900

    feat: 서브도메인 *으로 설정하기

--- pages/member/index.js
+++ pages/member/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import { useRouter } from 'next/router';
 
 export default function Member() {
-  return <div>Member</div>
+  const router = useRouter();
+  const { subdomain } = router.query;
+
+
+  return <div>Member: {subdomain}</div>
 }
--- server.js
+++ server.js
@@ -1,47 +1,60 @@
-const express = require('express')
-const next = require('next')
-const vhost = require('vhost')
+const express = require('express');
+const next = require('next');
+const vhost = require('vhost');
 
-const port = process.env.PORT || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const mainServer = express()
-  const adminServer = express()
-  const memberServer = express()
+  const mainServer = express();
+  const adminServer = express();
+  const memberServer = express();
 
   adminServer.get('/', (req, res) => {
-    return app.render(req, res, '/admin', req.query)
-  })
+    const subdomain = req.vhost[0].split('.')[0]; // 추출된 서브도메인 값
+    return app.render(req, res, '/admin', { subdomain });
+  });
 
   adminServer.get('/*', (req, res) => {
-    return app.render(req, res, `/admin${req.path}`, req.query)
-  })
+    const subdomain = req.vhost[0].split('.')[0]; // 추출된 서브도메인 값
+    return app.render(req, res, `/admin${req.path}`, { subdomain });
+  });
+
 
   adminServer.all('*', (req, res) => {
-    return handle(req, res)
-  })
+    return handle(req, res);
+  });
 
   memberServer.get('/', (req, res) => {
-    return app.render(req, res, '/member', req.query)
-  })
+    const subdomain = req.vhost[0].split('.')[0]; // 추출된 서브도메인 값
+    return app.render(req, res, '/member', { subdomain });
+  });
 
   memberServer.get('/*', (req, res) => {
-    return app.render(req, res, `/member${req.path}`, req.query)
-  })
+    const subdomain = req.vhost[0].split('.')[0]; // 추출된 서브도메인 값
+    return app.render(req, res, `/member${req.path}`, { subdomain });
+  });
 
   memberServer.all('*', (req, res) => {
-    return handle(req, res)
-  })
+    return handle(req, res);
+  });
 
-  mainServer.use(vhost('admin.lvh.me', adminServer))
-  mainServer.use(vhost('lvh.me', memberServer))
-  mainServer.use(vhost('www.lvh.me', memberServer))
+  mainServer.use(vhost('*.lvh.me', (req, res, next) => {
+    const subdomain = req.vhost[0].split('.')[0]; // 서브도메인 추출
+
+    if (subdomain === 'admin') {
+      return adminServer(req, res, next);
+    } else {
+      return memberServer(req, res, next);
+    }
+  }));
+
+
   mainServer.listen(port, (err) => {
-    if (err) throw err
+    if (err) throw err;
 
-    console.log(`> Ready on http://lvh.me:${port}`)
-  })
-})
+    console.log(`> Ready on http://lvh.me:${port}`);
+  });
+});
